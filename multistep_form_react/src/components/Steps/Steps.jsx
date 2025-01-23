b5573632a75b9cec197f29bd5b9e39157c1d7369
CaioBertoldo <caio23bezerra@gmail.com>
2025-01-23 13:55:53 -0400

    conclusão

---
+++ multistep_form_react/src/components/Steps/Steps.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import {AiOutlineUser, AiOutlineStar} from 'react-icons/ai'
+import { FiSend } from 'react-icons/fi'
+import './Steps.css'
+
+const Steps = ({ currentStep }) => {
+  return (
+    <div className='steps'>
+        <div className="step active">
+            <AiOutlineUser />
+            <p>Identidicação</p>
+        </div>
+        <div className={`step ${currentStep >= 1 ? "active" : ""}`}>
+            <AiOutlineStar />
+            <p>Avaliação</p>
+        </div>
+        <div className={`step ${currentStep >= 2 ? "active" : ""}`}>
+            <FiSend />
+            <p>Envio</p>
+        </div>
+    </div>
+  )
+}
+
+export default Steps
